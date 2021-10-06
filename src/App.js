19ad3cce0f873498e4f4b052fee124edebdb61b9
HarshGadage16 <62607923+HarshGadage16@users.noreply.github.com>
2021-10-06 11:28:39 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Competitions from "./Components/competitions/Competitions"
+
+export default function App() {
+  return (
+      <Competitions>
+      </Competitions>
+
+  );
+}
